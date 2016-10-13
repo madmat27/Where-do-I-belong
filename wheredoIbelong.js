aef3094ebf4488025c1150744bf891a8136ca20e
Mariana S. Mz <mscmazi@gmail.com>
2016-10-13 10:57:44 +0300

    v3.00
    Testing with new variables....

--- wheredoIbelong.js
+++ wheredoIbelong.js
@@ -3,8 +3,7 @@ function getIndexToIns(arr, num) {
   
   var board = Array.prototype.slice.call(arguments);
   var i;
-  var j;
-  
+
   for (i = 0; i < board.length; i++) {
     if (Array.isArray(board[i])) {
       board.splice(i,1);
@@ -19,8 +18,11 @@ function getIndexToIns(arr, num) {
         if (arr[i] ==  board[0]) {
           return i; 
         }
+        else if (arr[i] >  board[0]) {
+          return i; 
+        }
       }
     
 }
 
-getIndexToIns([5, 3, 20, 3], 5);
+getIndexToIns([10, 20, 30, 40, 50], 35);
